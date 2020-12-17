f453426e70df5e4244958b15cc8d351d938777ce
Simon Lindholm <simon.lindholm10@gmail.com>
2020-12-17 19:20:32 +0100

    Cleanup

--- 17.cpp
+++ 17.cpp
@@ -18,8 +18,7 @@ int main() {
 	int pad = iters + 1;
 	int M = N + 2*pad;
 
-	vector<vector<vector<string>>> state(M, vector<vector<string>>(M, vector<string>(M, string(M, '.'))));
-	vector<vector<vector<string>>> state2(M, vector<vector<string>>(M, vector<string>(M, string(M, '.'))));
+	vector<vector<vector<string>>> state(M, vector<vector<string>>(M, vector<string>(M, string(M, '.')))), next = state;
 	rep(i,0,N) rep(j,0,N) state[pad][pad][pad + i][pad + j] = v[i][j];
 
 	rep(it,0,iters) {
@@ -27,9 +26,9 @@ int main() {
 			int co = 0;
 			rep(dw,-1,2) rep(dx,-1,2) rep(dy,-1,2) rep(dz,-1,2) 
 				co += state[l + dw][i + dx][j + dy][k + dz] == '#';
-			state2[l][i][j][k] = (state[l][i][j][k] == '#' ? (co == 3 || co == 4) : (co == 3)) ? '#' : '.';
+			next[l][i][j][k] = (state[l][i][j][k] == '#' ? (co == 3 || co == 4) : (co == 3)) ? '#' : '.';
 		}
-		state.swap(state2);
+		state.swap(next);
 	}
 
 	int res = 0;
